e7ce28dae8e5fdf5f00a6a6bb5806f021b5a2788
BimsaraG <bimsaragayathraya@gmail.com>
2024-01-18 23:19:14 +0530

    Indicator and Status boxes are coded.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,6 +40,8 @@
             this.txtBMI = new System.Windows.Forms.TextBox();
             this.btnCalculate = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -148,6 +150,27 @@
             this.button1.TabIndex = 2;
             this.button1.Text = "Reset";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft JhengHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(311, 111);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 20);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Kg";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft JhengHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(311, 60);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 20);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "m";
             // 
             // Form1
             // 
@@ -165,6 +188,8 @@
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "BMI Calculator";
@@ -187,6 +212,8 @@
         private System.Windows.Forms.TextBox txtBMI;
         private System.Windows.Forms.Button btnCalculate;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -29,6 +30,43 @@ namespace BMI_value
             weight = double.Parse (txtWeight.Text);
             bmi = weight / (height * height);
             txtBMI.Text=bmi.ToString();
+
+
+            if (bmi > 18.5)
+            {
+                textBox2.Text = "Under Weighnt";
+                txtIndicator.BackColor = Color.Red;
+            }
+            else if (bmi < 25)
+            {
+                textBox2.Text = "Normal";
+                txtIndicator.BackColor = Color.Blue;
+            }
+            else if (bmi < 30)
+            {
+                textBox2.Text =  "Obesity class I";
+                txtIndicator.BackColor = Color.Orange;
+            }
+            else if (bmi < 40)
+            {
+                textBox2.Text = "Obesity class II";
+                txtIndicator.BackColor = Color.Pink;
+            }
+            else 
+            {
+                textBox2.Text = "Obesity class III";
+                txtIndicator.BackColor = Color.Brown;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            txtWeight.Clear();
+            txtIndicator.BackColor = Color.White;
+            txtHeight.Clear();
+            txtBMI.Clear();
 
         }
     }
